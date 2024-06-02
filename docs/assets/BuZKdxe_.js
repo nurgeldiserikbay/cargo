dced3b6d98c5f17611cb62b0ab516f6f78fb259b
nurgeldiserikbay <nurikserikbay1995@gmail.com>
2024-06-02 22:31:59 +0500

    save

---
+++ docs/assets/BuZKdxe_.js
@@ -0,0 +1,1 @@
+import{d as e,o,s}from"./DYPdmrsr.js";const t={class:""},r=e({__name:"index",setup(n){return(a,c)=>(o(),s("div",t))}});export{r as default};
--- docs/assets/CV61f7H2.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o,s}from"./enCeUreN.js";const t={class:""},r=e({__name:"index",setup(n){return(a,c)=>(o(),s("div",t))}});export{r as default};
